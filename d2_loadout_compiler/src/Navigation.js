bb612547f38d400cfab0ba288450a19f9318cbc9
sid12c <sid12c@uga.edu>
2023-05-24 20:19:02 -0400

    added basic nav bar

---
+++ d2_loadout_compiler/src/Navigation.js
@@ -0,0 +1,30 @@
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <div classname='nav-bar'>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                Character Select
+            </li>
+            <li>
+                Damage Count
+            </li>
+            <li>
+                Loadouts
+            </li>
+            <li>
+                About
+            </li>
+        </ul>
+        <div classname='login-button'>
+            <></>
+        </div>
+    </div>
+  );
+}
+
+export default Navigation;
